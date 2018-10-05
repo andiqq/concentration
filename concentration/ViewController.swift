651073007cb8d15c14e08abe85f299cc953d8f15
Andreas Plagens <andreas@macbookpro.local>
2018-10-06 00:51:51 +0200

    erste Version 2 Karten, die gedreht werden können, Anzahl wird gezählt

--- concentration/ViewController.swift
+++ concentration/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
 
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
 
 }
 
